c570ac9d0900f3f86dc78d435cfb204974f06b5b
sainikunal5950-sudo <sainikunal5950@gmail.com>
2026-07-20 22:31:51 +0530

    database connect

---
+++ lib/dbConnect.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number
+}
+const connection: ConnectionObject = {}
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already Database is Connected ");
+        return;
+    }
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+        connection.isConnected = db.connections[0].readyState
+        console.log("DB connected successfully");
+    } catch (error) {
+        console.log("Dtatabase connection false", error);
+        process.exit(1);
+    }
+}
+
+export default dbConnect;
