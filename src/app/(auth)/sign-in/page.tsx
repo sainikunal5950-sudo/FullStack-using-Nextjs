33f3a4d70c3e362a57ee6c44f2de03613042ea21
sainikunal5950-sudo <sainikunal5950@gmail.com>
2026-08-03 23:59:53 +0530

    logic of page.tsx

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,28 +1,21 @@
-'use client'
-import { useSession, signIn, signOut } from "next-auth/react";
+'use client'  // eska main mtlb next js mai kya hota hai hrr comcpeoonet server pe rnder hota hai aur react mai browser agrr tum use satte vgra render krna chata ho toh tumko 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useDebounceValue } from "usehooks-ts";
 
-export default function Component() {
-    const { data: session } = useSession();
 
-    if (session) {
-        return (
-            <>
-                Signed in as {session.user.email}
-                <br />
-                <button onClick={() => signOut()}>
-                    Sign out
-                </button>
-            </>
-        );
-    }
 
+const page = () => {
+    const [username, setUsername] = useState('');
+    const [usernamemessage, setusernameMessage] = useState('');
+    const [ischeckingUsername, setischeckingUsername] = useState(false);
+    const [issubmitting, setissubmitting] = useState(false);
+    const debouncedusername = useDebounceValue(username, 300);
     return (
-        <>
-            Not signed in
-            <br />
-            <button onClick={() => signIn()}>
-                Sign in
-            </button>
-        </>
-    );
+        <div>page</div>
+    )
 }
+export default page
